c6e6be739ac84275ff2ef51d7feb12340014dbdc
Anna Belyaeva <aabelyaeva2@stud.eltech.ru>
2023-10-11 16:57:06 +0300

    Making list of books more accurate

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -1,30 +1,30 @@
 import Link from 'next/link'; // Для создания ссылок между страницами
 import styles from './BookList.module.css'; // Импортируем стили
 import booksData from '../../data/books.json'; // Импортируем данные о книгах
-import { Col, Container, Image } from 'react-bootstrap'; // Импортируем компоненты react-bootstrap
+import { Col, Container, Image, Row } from 'react-bootstrap'; // Импортируем компоненты react-bootstrap
 import '@/styles/globals.css'
 
 const BookList = () => {
   return (
     <div className={styles.bookList}>
       <h2 className={styles.header}>Список книг</h2>
-      {booksData.map((book) => (
-        <Col key={book.id}>
-          <Container className={`${styles.list_el} flex_direction_row`}>
-            <Col>
-              <Image className={styles.img_cover} src={book.cover} />
-            </Col>
-            <Col>
-              <Link href={`/book/${book.id}`} className={styles.bookLink}>
-                <h3>{book.title}</h3>
-                <p>Автор: {book.author}</p>
-                <p>Описание : {book.description}</p>
-              </Link>
-            </Col>
-          </Container>
-          {/* Ссылка на страницу с детальной информацией о книге */}
-        </Col>
-      ))}
+      <Container className={`row ${styles.table}`}>
+        {booksData.map((book, index) => (
+          <Col key={index} className='col-md-3 mb-4'>
+            <Container className={`${styles.list_el} `}>
+              <Col>
+                <Image className={styles.img_cover} src={book.cover} />
+              </Col>
+              <Col>
+                <Link href={`/book/${book.id}`} className={styles.bookLink}>
+                  <p><b>{book.title}</b></p>
+                  <p>{book.author}</p>
+                </Link>
+              </Col>
+            </Container>
+          </Col>
+        ))}
+      </Container>
     </div>
   );
 };
